aa7c59c12cbdb9b81b7f6a5f650bb2ccaff0c477
dsc <dsc@less.ly>
2012-11-26 23:52:40 -0800

    Updates module handling; fixes op.bin()

--- operator.js
+++ operator.js
@@ -1,298 +1,300 @@
-var DASH_PATTERN, STRIP_PAT, FALSEY, parseBool, op, __slice = [].slice;
+var DASH_PATTERN, STRIP_PAT, FALSEY, op, slice$ = [].slice;
 DASH_PATTERN = /-/g;
 STRIP_PAT = /(^\s*|\s*$)/g;
 FALSEY = /^\s*(?:no|off|false)\s*$/i;
-parseBool = function(s){
+op = {};
+if (typeof exports != 'undefined' && exports !== null) {
+  op = exports;
+} else if (typeof window != 'undefined' && window !== null) {
+  window.operator = op;
+}
+if (typeof define === 'function' && define.amd) {
+  define('operator', [], function(require, exports, module){
+    return module.exports = op;
+  });
+}
+op.I = function(x){
+  return x;
+};
+op.K = function(k){
+  return function(){
+    return k;
+  };
+};
+op.nop = function(){};
+op.noop = op.noop;
+op.kThis = function(){
+  return this;
+};
+op.kObject = function(){
+  return {};
+};
+op.kArray = function(){
+  return [];
+};
+op.val = function(def, o){
+  return o != null ? o : def;
+};
+op.ok = function(o){
+  return o != null;
+};
+op.notOk = function(o){
+  return o == null;
+};
+op.isK = function(k){
+  return function(v){
+    return v === k;
+  };
+};
+op.second = function(_, a){
+  return a;
+};
+op.nth = function(n){
+  switch (n) {
+  case 0:
+    return op.first;
+  case 1:
+    return op.second;
+  default:
+    return function(){
+      return arguments[n];
+    };
+  }
+};
+op.flip = function(fn){
+  return function(a, b){
+    arguments[0] = b;
+    arguments[1] = a;
+    return fn.apply(this, arguments);
+  };
+};
+op.aritize = function(fn, cxt, n){
+  var ref$;
+  if (arguments.length < 3) {
+    ref$ = [cxt, null], n = ref$[0], cxt = ref$[1];
+  }
+  return function(){
+    return fn.apply(cxt != null ? cxt : this, [].slice.call(arguments, 0, n));
+  };
+};
+op.it = function(fn, cxt){
+  return function(it){
+    return fn.call(cxt != null ? cxt : this, it);
+  };
+};
+op.khas = function(k, o){
+  return k in o;
+};
+op.kget = function(k, o){
+  return o[k];
+};
+op.defkget = function(def, k, o){
+  if (k in o) {
+    return o[k];
+  } else {
+    return def;
+  }
+};
+op.thisget = function(k){
+  return this[k];
+};
+op.vkset = function(o, v, k){
+  if (o && k != null) {
+    o[k] = v;
+  }
+  return o;
+};
+op.has = function(o, k){
+  return k in o;
+};
+op.get = function(o, k){
+  return o[k];
+};
+op.getdef = function(o, k, def){
+  if (k in o) {
+    return o[k];
+  } else {
+    return def;
+  }
+};
+op.kvset = function(o, k, v){
+  if (o && k != null) {
+    o[k] = v;
+  }
+  return o;
+};
+op.thiskvset = function(k, v){
+  if (k != null) {
+    this[k] = v;
+  }
+  return this;
+};
+op.prop = function(k){
+  return function(o){
+    return o[k];
+  };
+};
+op.method = function(name){
+  var args;
+  args = slice$.call(arguments, 1);
+  return function(obj){
+    var _args;
+    _args = slice$.call(arguments, 1);
+    if (obj != null && obj[name]) {
+      return obj[name].apply(obj, args.concat(_args));
+    }
+  };
+};
+op.parseBool = function(s){
   var i;
   i = parseInt(s || 0);
   return !!(isNaN(i) ? !FALSEY.test(s) : i);
 };
-op = {
-  I: function(x){
-    return x;
-  },
-  K: function(k){
-    return function(){
-      return k;
-    };
-  },
-  nop: function(){},
-  kThis: function(){
-    return this;
-  },
-  kObject: function(){
-    return {};
-  },
-  kArray: function(){
-    return [];
-  },
-  val: function(def, o){
-    return o != null ? o : def;
-  },
-  ok: function(o){
-    return o != null;
-  },
-  notOk: function(o){
-    return o == null;
-  },
-  isK: function(k){
-    return function(v){
-      return v === k;
-    };
-  },
-  second: function(_, a){
-    return a;
-  },
-  nth: function(n){
-    switch (n) {
-    case 0:
-      return op.first;
-    case 1:
-      return op.second;
-    default:
-      return function(){
-        return arguments[n];
-      };
-    }
-  },
-  flip: function(fn){
-    return function(a, b){
-      arguments[0] = b;
-      arguments[1] = a;
-      return fn.apply(this, arguments);
-    };
-  },
-  aritize: function(fn, cxt, n){
-    var __ref;
-    if (arguments.length < 3) {
-      __ref = [cxt, null], n = __ref[0], cxt = __ref[1];
-    }
-    return function(){
-      return fn.apply(cxt != null ? cxt : this, [].slice.call(arguments, 0, n));
-    };
-  },
-  it: function(fn, cxt){
-    return function(it){
-      return fn.call(cxt != null ? cxt : this, it);
-    };
-  },
-  khas: function(k, o){
-    return k in o;
-  },
-  kget: function(k, o){
-    return o[k];
-  },
-  defkget: function(def, k, o){
-    if (k in o) {
-      return o[k];
+op.toBool = op.parseBool;
+op.toInt = function(v){
+  return parseInt(v);
+};
+op.toFloat = function(v){
+  return parseFloat(v);
+};
+op.toStr = function(v){
+  return String(v);
+};
+op.toRegExp = function(v){
+  return new RegExp(v);
+};
+op.toObject = function(v){
+  if (typeof v === 'string' && op.strip(v)) {
+    return JSON.parse(v);
+  } else {
+    return Object(v);
+  }
+};
+op.toDate = function(v){
+  if (v == null || v instanceof Date) {
+    return v;
+  }
+  if (typeof v === 'number') {
+    return new Date(v);
+  }
+  return new Date(String(v).replace(DASH_PATTERN, '/'));
+};
+op.cmp = function(x, y){
+  if (x < y) {
+    return -1;
+  } else {
+    if (x > y) {
+      return 1;
     } else {
-      return def;
-    }
-  },
-  thisget: function(k){
-    return this[k];
-  },
-  vkset: function(o, v, k){
-    if (o && k != null) {
-      o[k] = v;
-    }
-    return o;
-  },
-  has: function(o, k){
-    return k in o;
-  },
-  get: function(o, k){
-    return o[k];
-  },
-  getdef: function(o, k, def){
-    if (k in o) {
-      return o[k];
-    } else {
-      return def;
-    }
-  },
-  kvset: function(o, k, v){
-    if (o && k != null) {
-      o[k] = v;
-    }
-    return o;
-  },
-  thiskvset: function(k, v){
-    if (k != null) {
-      this[k] = v;
-    }
-    return this;
-  },
-  prop: function(k){
-    return function(o){
-      return o[k];
-    };
-  },
-  method: function(name){
-    var args;
-    args = __slice.call(arguments, 1);
-    return function(obj){
-      var _args;
-      _args = __slice.call(arguments, 1);
-      if (obj != null && obj[name]) {
-        return obj[name].apply(obj, args.concat(_args));
-      }
-    };
-  },
-  parseBool: parseBool,
-  toBool: parseBool,
-  toInt: function(v){
-    return parseInt(v);
-  },
-  toFloat: function(v){
-    return parseFloat(v);
-  },
-  toStr: function(v){
-    return String(v);
-  },
-  toRegExp: function(v){
-    return new RegExp(v);
-  },
-  toObject: function(v){
-    if (typeof v === 'string' && op.strip(v)) {
-      return JSON.parse(v);
-    } else {
-      return v;
-    }
-  },
-  toDate: function(v){
-    if (v == null || v instanceof Date) {
-      return v;
-    }
-    if (typeof v === 'number') {
-      return new Date(v);
-    }
-    return new Date(String(v).replace(DASH_PATTERN, '/'));
-  },
-  cmp: function(x, y){
-    if (x < y) {
-      return -1;
-    } else {
-      if (x > y) {
-        return 1;
-      } else {
-        return 0;
-      }
-    }
-  },
-  eq: function(x, y){
-    return x == y;
-  },
-  ne: function(x, y){
-    return x != y;
-  },
-  gt: function(x, y){
-    return x > y;
-  },
-  ge: function(x, y){
-    return x >= y;
-  },
-  lt: function(x, y){
-    return x < y;
-  },
-  le: function(x, y){
-    return x <= y;
-  },
-  add: function(x, y){
-    return x + y;
-  },
-  sub: function(x, y){
-    return x - y;
-  },
-  mul: function(x, y){
-    return x * y;
-  },
-  div: function(x, y){
-    return x / y;
-  },
-  flrdiv: function(x, y){
-    return Math.floor(x / y);
-  },
-  mod: function(x, y){
-    return x % y;
-  },
-  neg: function(x){
-    return -x;
-  },
-  log2: function(n){
-    return Math.log(n / Math.LN2);
-  },
-  is: function(x, y){
-    return x === y;
-  },
-  isnt: function(x, y){
-    return x !== y;
-  },
-  and: function(x, y){
-    return x && y;
-  },
-  or: function(x, y){
-    return x || y;
-  },
-  not: function(x){
-    return !x;
-  },
-  bitnot: function(x){
-    return ~x;
-  },
-  bitand: function(x, y){
-    return x & y;
-  },
-  bitor: function(x, y){
-    return x | y;
-  },
-  bitxor: function(x, y){
-    return x ^ y;
-  },
-  lshift: function(x, y){
-    return x << y;
-  },
-  rshift: function(x, y){
-    return x >> y;
-  },
-  bin: function(n){
-    var s;
-    do {
-      s = (n % 2 ? '1' : '0') + (s || '');
-      n >>= 1;
-    } while (n);
-    return s;
-  },
-  binlen: function(n){
-    return bin(Math.abs(n)).length;
-  },
-  mask: function(n){
-    return (1 << n) - 1;
-  },
-  chr: function(it){
-    return String.fromCharCode(it);
-  },
-  ord: function(it){
-    return String(it).charCodeAt(0);
-  },
-  encode: function(it){
-    return it && $("<div>" + it + "</div>").html().replace(/"/g, '&quot;');
-  },
-  decode: function(it){
-    return it && $("<div>" + it + "</div>").text();
-  },
-  strip: function(s){
-    if (s) {
-      return s.replace(STRIP_PAT, '');
-    } else {
-      return s;
+      return 0;
     }
   }
 };
-if (typeof module != 'undefined' && module !== null) {
-  if (typeof module != 'undefined' && module !== null) {
-    module.exports = op;
+op.eq = function(x, y){
+  return x == y;
+};
+op.ne = function(x, y){
+  return x != y;
+};
+op.gt = function(x, y){
+  return x > y;
+};
+op.ge = function(x, y){
+  return x >= y;
+};
+op.lt = function(x, y){
+  return x < y;
+};
+op.le = function(x, y){
+  return x <= y;
+};
+op.add = function(x, y){
+  return x + y;
+};
+op.sub = function(x, y){
+  return x - y;
+};
+op.mul = function(x, y){
+  return x * y;
+};
+op.div = function(x, y){
+  return x / y;
+};
+op.flrdiv = function(x, y){
+  return Math.floor(x / y);
+};
+op.mod = function(x, y){
+  return x % y;
+};
+op.neg = function(x){
+  return -x;
+};
+op.log2 = function(n){
+  return Math.log(n / Math.LN2);
+};
+op.is = function(x, y){
+  return x === y;
+};
+op.isnt = function(x, y){
+  return x !== y;
+};
+op.and = function(x, y){
+  return x && y;
+};
+op.or = function(x, y){
+  return x || y;
+};
+op.not = function(x){
+  return !x;
+};
+op.bitnot = function(x){
+  return ~x;
+};
+op.bitand = function(x, y){
+  return x & y;
+};
+op.bitor = function(x, y){
+  return x | y;
+};
+op.bitxor = function(x, y){
+  return x ^ y;
+};
+op.lshift = function(x, y){
+  return x << y;
+};
+op.rshift = function(x, y){
+  return x >> y;
+};
+op.bin = function(n){
+  var s;
+  do {
+    s = (n % 2 ? '1' : '0') + (s || '');
+    n >>= 1;
+  } while (n);
+  return s;
+};
+op.binlen = function(n){
+  return op.bin(Math.abs(n)).length;
+};
+op.mask = function(n){
+  return (1 << n) - 1;
+};
+op.chr = function(it){
+  return String.fromCharCode(it);
+};
+op.ord = function(it){
+  return String(it).charCodeAt(0);
+};
+op.encode = function(it){
+  return it && $("<div>" + it + "</div>").html().replace(/"/g, '&quot;');
+};
+op.decode = function(it){
+  return it && $("<div>" + it + "</div>").text();
+};
+op.strip = function(s){
+  if (s) {
+    return s.replace(STRIP_PAT, '');
+  } else {
+    return s;
   }
-} else if (typeof window != 'undefined' && window !== null) {
-  window.operator = op;
-}
+};
